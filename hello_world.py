bf9ee8bf2361e198c44eefdcabeb2a2dbde844fa
Dustin <133918287+dustinmosley@users.noreply.github.com>
2023-05-19 13:01:32 -0400

    Variable & Function test
    Case study for basic variables and functions.

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+
+# Split user's name into first and last name
+first, last = name.split(" ")
+
+# Say hello to user
+print(f"Hello, {first}")
+
+"""
+Multi-line comment
+"""
